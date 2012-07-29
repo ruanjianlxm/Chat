ac12d8918af17ca79fbdee3f1a05fccd4de7956a
刘小明 <657874721@qq.com>
2012-07-29 13:15:09 +0800

    基本完成小会话

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -7,24 +7,60 @@ public class ChatServer {
       
 	public static void main(String[] args) {
 		   boolean started =false;
+		   ServerSocket ss=null;
+		   Socket s = null;
+		   DataInputStream dis =null;
 		try {
-			ServerSocket ss = new ServerSocket(8882);
+			 ss= new ServerSocket(8882);
+		}catch (IOException e) {
+			e.printStackTrace();
+			// TODO: handle exception
+		}
+		try{
+			
 			started =true;
 			while (started) {
 				boolean bconnected =false;
-				Socket s = ss.accept();
+				s = ss.accept();
 				System.out.println("a client connected!");
 			    bconnected =true;
-				DataInputStream dis = new DataInputStream(s.getInputStream());
+				dis = new DataInputStream(s.getInputStream());
 				while(bconnected){
 				String str = dis.readUTF();
 				System.out.println(str);
 				}
-				dis.close();
+		
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
+			
+			
+		} 
+		
+		
+		catch (EOFException e) {
+			// TODO: handle exception
+			System.out.println("client closed");
 		}
+		catch (Exception e) {
+			e.printStackTrace();
+	
+			//e.printStackTrace();
+		
+		}
+		
+		finally{
+			try {
+				if(dis!=null)
+				dis.close();
+				if(s!=null)
+				s.close();
+				
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+		}
+		
 	}
 
 }
